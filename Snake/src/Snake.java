6b47f9a62216f9dbf6ae110d33a1aebf34ba8294
Yassin306 <45129763+Yassin306@users.noreply.github.com>
2018-11-18 00:51:13 +0100

    Added snake clase that stores the pos of a snake

---
+++ Snake/src/Snake.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * @author 
+ *
+ */
+public class Snake {
+	private int x;
+	private int y;
+	/**
+	 * 
+	 */
+	public Snake(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+	/**
+	 * @param x the x to set
+	 * @param y the y to set
+	 */
+	public void setPos(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
