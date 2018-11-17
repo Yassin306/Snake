ebcd086eb7f83c73dec8c3de857222439421b9cd
Yassin306 <45129763+Yassin306@users.noreply.github.com>
2018-11-17 22:29:30 +0100

    Adding Controller class

---
+++ Snake/src/Controller.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author 
+ *
+ */
+public class Controller {
+	public enum Direction{
+		UP,DOWN,LEFT,RIGHT
+	}
+	private Direction direction;
+	
+	/**
+	 * 
+	 */
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the direction
+	 */
+	public Direction getDirection() {
+		return direction;
+	}
+
+}
