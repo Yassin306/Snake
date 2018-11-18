fa977a3710707a261f84102f7a5b0e75f83d4f90
MatiasVigilione <45129776+MatiasVigilione@users.noreply.github.com>
2018-11-18 04:49:30 +0100

    Add CheckObjective() and NewObjective() in the Game class

--- Snake/src/Game.java
+++ Snake/src/Game.java
@@ -8,15 +8,19 @@
  */
 public class Game {
 	private Board board;
+	int rowSize;
+	int colSize;
 	
 	public Game(int rowSize, int colSize) {
-		start(rowSize, colSize);
+		this.rowSize = rowSize;
+		this.colSize = colSize;
+		start();
 	}
 	
 	/**
 	 * Game start
 	 */
-	public void start(int rowSize, int colSize) {
+	public void start() {
 		board = new Board(rowSize, colSize);
 		board.setHead((int) (rowSize/2), (int) (colSize/2));
 		board.setCells((int)(Math.random() * rowSize), (int)(Math.random() * colSize), Cell.CellType.ITEM);
@@ -58,5 +62,54 @@ public class Game {
 		}
 		
 		return collision;
+	}
+	
+	/**
+	 * look if the snake has collected the objective
+	 * and create a new Objective
+	 */
+	public void CheckObjective(Controller.Direction direction) {
+		int pos_X = board.getHead().getX();
+		int pos_Y = board.getHead().getY();
+		switch (direction) {
+		case UP:
+			if (board.getCell(pos_X, pos_Y - 1) == Cell.CellType.ITEM) {
+				board.addBody(direction);
+				NewObjective();
+			}
+			break;
+		case DOWN:
+			if (board.getCell(pos_X, pos_Y + 1) == Cell.CellType.ITEM) {
+				board.addBody(direction);
+				NewObjective();
+			}
+			break;
+		case LEFT:
+			if (board.getCell(pos_X - 1, pos_Y) == Cell.CellType.ITEM) {
+				board.addBody(direction);
+				NewObjective();
+			}
+			break;
+		case RIGHT:
+			if (board.getCell(pos_X + 1, pos_Y) == Cell.CellType.ITEM) {
+				board.addBody(direction);
+				NewObjective();
+			}
+			break;
+		}
+	}
+	
+	/**
+	 * create a new Objective
+	 * 
+	 */
+	public void NewObjective() {
+		int pos_x = (int)(Math.random() * rowSize);
+		int pos_y = (int)(Math.random() * colSize);
+		if (board.getCell(pos_x, pos_y) != Cell.CellType.SNAKE) {
+			board.setCells(pos_x, pos_y, Cell.CellType.ITEM);
+		} else {
+			NewObjective();
+		}
 	}
 }
